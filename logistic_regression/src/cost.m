f845a8a08a0c97f34254d71188d7921f88323e41
Thibault <tallancon@gmail.com>
2016-03-16 12:57:12 +0100

    Improved gradient calculation in logistic regression (added vectorization)

--- logistic_regression/src/cost.m
+++ logistic_regression/src/cost.m
@@ -9,9 +9,8 @@ reg = (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
 J = (1 / m) * sum(-y .* log(h) - (1 .- y) .* log(1 .- h)) + reg;
 
 % theta0 remains unchanged with regularization, but theta0 is actually theta(1)
-gradient(1) = (1 / m) * sum((h - y) .* X(:,1));
-for j = 2:length(theta)
-   gradient(j) = ((1 / m) * sum((h - y) .* X(:,j))) + (lambda / m) .* theta(j);
-end
+tempT = theta;
+tempT(1) = 0;
+gradient = (1 / m) * sum((h - y) .* X) + (lambda / m) .* pinv(tempT);
 
 end
